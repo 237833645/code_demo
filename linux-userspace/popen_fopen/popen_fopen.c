f980c07966ec500d644cae1d57c89cdba7e5ee30
237833645 <237833645@qq.com>
2021-06-21 18:51:40 +0800

    20210621

--- linux-userspace/popen_fopen/popen_fopen.c
+++ linux-userspace/popen_fopen/popen_fopen.c
@@ -1,14 +1,32 @@
+#include "stdlib.h"
 #include "stdio.h"
 #include "string.h"
 
+char* fifo_read(char *str, char *attr)
+{
+  FILE *fifo;    // shell管道
+  char buf[10240]; //获取的shell输出存储buf
+  memset(buf, '\0', sizeof(buf));
+  fifo = fopen(str, attr);
+  if (fifo == NULL)
+  {
+    printf("Can't connect to pipe\n");
+    return -1;
+  }
+
+  fread(buf, sizeof(char), sizeof(buf), fifo);
+  printf("%s", buf);
+  return buf;
+}
+
 int main(int args, char *argv[]) {
+  FILE *fifo;    // shell管道
+  char buf[10240]; //获取的shell输出存储buf
+
   if (args > 1) {
     printf("Usage: %s command\n", argv[0]);
   }
   printf("%s\n", argv[1]);
-
-  FILE *fifo;    // shell管道
-  char buf[10240]; //获取的shell输出存储buf
 
   memset(buf, '\0', sizeof(buf));
 
